69fbc6be5f295b2ada3545274777eb55d480d114
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-05-27 03:32:27 +0000

    association User to Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30} 
   validates :price, presence: true 
+  
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false},
                     format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   has_secure_password
+  
+  has_many :items
 end
