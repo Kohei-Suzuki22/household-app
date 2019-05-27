ed30db4a9511f0a5ce5cf521e4f281231e173bc1
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-05-27 18:12:17 +0000

    toppage views

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,5 +3,10 @@ class ToppagesController < ApplicationController
   skip_before_action :login_required
   
   def index
+    if logged_in?
+      @budget = current_user.budget
+      @expense = current_user.items.sum(:price)
+      @difference = @budget - @expense unless @budget.blank?
+    end
   end
 end
