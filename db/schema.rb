c6996302340b4f80ce35dcc58a7f68ad71e6638a
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-05-27 06:11:33 +0000

    add_category_to_item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_032232) do
+ActiveRecord::Schema.define(version: 2019_05_27_055612) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_032232) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
