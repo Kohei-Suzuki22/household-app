49a3dbb5e154683a6cc109dee494cf26920fe3a0
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-05-28 06:21:08 +0000

    set zero if user_budget is null

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save {self.email.downcase!}
+  before_validation :set_zero_if_budget_null
   
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false},
@@ -7,4 +8,8 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :items, dependent: :destroy
+  
+  def set_zero_if_budget_null 
+    self.budget = 0 if budget.blank?
+  end 
 end
